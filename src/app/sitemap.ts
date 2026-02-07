86912b1fb127ecc74a0c7b223600985c985b8852
rudresh990 <rudresh990@gmail.com>
2026-02-07 17:49:28 +0530

    update(seo):sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -33,7 +33,7 @@ export default function sitemap():MetadataRoute.Sitemap{
         priority:0.6,
     },
     {
-        url:`${baseUrl}/estimate`,
+        url:`${baseUrl}/software-development-cost-estimator`,
         lastModified:new Date(),
         changeFrequency:"monthly",
         priority:0.6,
