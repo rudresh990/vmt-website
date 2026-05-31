4cd79e3c33f54dcbcd2be8399ef618fb9413e93f
rudresh990 <rudresh990@gmail.com>
2026-05-31 13:18:25 +0530

    feat:added whatsapp button

--- src/components/common/WhatsAppButton.tsx
+++ src/components/common/WhatsAppButton.tsx
@@ -1,6 +1,5 @@
 'use client';
-
-import { MessageCircle, MessageCircleMore } from 'lucide-react';
+import { ImWhatsapp } from 'react-icons/im';
 
 export default function WhatsAppButton() {
   const phone = '919321446746';
@@ -17,11 +16,11 @@ export default function WhatsAppButton() {
       aria-label="Chat on WhatsApp"
       className="fixed bottom-6 right-6 z-50
       rounded-full p-4 shadow-lg
-    hover:scale-110
+      hover:scale-105 hover:shadow-[0_0_25px_rgba(34,197,94,0.7)] hover:border border-green-300
     text-white
-      transition"
+      transition-all duration-200"
     >
-      <MessageCircleMore className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 text-white" />
+      <ImWhatsapp className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 text-white" />
     </a>
   );
 }
